72b3bd43a2cdc5100c791e9a35a6251ff885a111
stphnieD <stphnie.dang@gmail.com>
2020-12-17 19:37:41 -0500

    Updated exercises

--- personal/js/tutorialScript.js
+++ personal/js/tutorialScript.js
@@ -8,24 +8,42 @@ let pos = 0;
 let dinoH = 0;
 let dinoV = 0;
 
+let bunH = 0;
+let bunV = 0;
+
 let sImg = d.getElementById(`thegirl`);
 let sImg2 = d.getElementById(`thedino`);
+let sImg3 = d.getElementById(`thebunny`);
+let flexElems = d.getElementsByClassName("flexelem");
 
 let dinoMadeIt = false;
 let dinoDrag = false;
 
+let bunVvel = 0;
+let bunHVel = 0;
+
 setInterval(movePicture, 20);
+setInterval(updatePos, 20);
 d.addEventListener("keydown", keyboard);
-d.addEventListener("keyup",onKeyUp);
+d.addEventListener("keyup", onKeyUp);
 d.addEventListener("mousemove", onMouseMove);
 
+if (typeof (window.localStorage) != "undefined") {
+    bunH = parseInt(window.localStorage.getItem("bunH"));
+    bunV = parseInt(window.localStorage.getItem("bunV"));
+}
 
 // FUNCTIONS ---------------------------------------------------------------------------------
 function onLoad() {
     sImg.style.position = "relative";
     sImg2.style.position = "absolute"; //for click and drag
+    sImg3.style.position = "relative";
 
-    sImg2.addEventListener("dragstart",cancelDrag);
+    sImg2.addEventListener("dragstart", cancelDrag);
+
+
+    sImg3.style.left = bunH + "px";
+    sImg3.style.top = bunV + "px";
 }
 
 function myFunction() {
@@ -41,31 +59,41 @@ function movePicture() {
     } else {
         pos++;
         sImg.style.left = pos + "px";
+
+        for (let i = 0; i < flexElems.length; i++) {
+            flexElems[i].style.left = pos + "px";
+        }
     }
 }
 
-function keyboard(param) {
-    if (param.keyCode == 65) {
-        dinoH -= 5;
-    } else if (param.keyCode == 68) {
-        dinoH += 5;
-    } else if (param.keyCode == 87) {
-        dinoV -= 5;
-    } else if (param.keyCode == 83) {
-        dinoV += 5;
-
+function keyboard(parameter) {
+    // left
+    if (parameter.keyCode == 65) {
+        bunHVel = -1
+    } else if (parameter.keyCode == 68) {
+        bunHVel += 1;
+    } else if (parameter.keyCode == 87) {
+        bunVvel -= 1;
+    } else if (parameter.keyCode == 83) {
+        bunVvel += 1;
     }
-    sImg2.style.left = dinoH + "px";
-    sImg2.style.top = dinoV + "px";
 
-    if (dinoH >= 300 && !dinoMadeIt) {
+    // sImg3.style.left = bunH + "px";
+    // sImg3.style.top = bunV + "px";
+
+    if (bunH >= 300 && !dinoMadeIt) {
         window.alert("u made it");
         dinoMadeIt = true;
     }
 }
 
-function onKeyUp() {
-    
+function onKeyUp(parameter) {
+    if (parameter == 65 || parameter == 68) {
+        bunHVel = 0;
+    } else if (parameter == 87 || parameter == 83) {
+        bunVvel = 0;
+    }
+
 }
 
 function drag() {
@@ -81,14 +109,24 @@ function cancelDrag() {
     return false;
 }
 
+function updatePos() {
+
+    bunH += bunHVel;
+    bunV += bunVvel;
+
+    sImg3.style.left = bunH + "px";
+    sImg3.style.top = bunV + "px";
+
+    window.localStorage.setItem("bunH", bunH);
+    window.localStorage.setItem("bunV", bunV);
+}
+
 function onMouseMove(event) {
-    if(dinoDrag) {
-        dinoV = event.pageY;
-        dinoH = event.pageX;
+    if (dinoDrag) {
+        dinoV = event.pageY - 75;
+        dinoH = event.pageX - 75;
 
         sImg2.style.left = dinoH + "px";
         sImg2.style.top = dinoV + "px";
     }
-
-
 }
